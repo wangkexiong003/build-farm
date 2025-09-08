7a9983030fb5f11fa5365353dbd98dbf09f44765
wangkexiong <wangkexiong@gmail.com>
2025-09-08 17:48:13 +0800

    debug2

--- openwrt/setup.sh
+++ openwrt/setup.sh
@@ -4,5 +4,6 @@ OPENWRT_SDK="$1"
 SCRIPTS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 for SCRIPT in "${SCRIPTS_DIR}/patches/patch*.sh"; do
+  echo "+++++ process ${SCRIPT}..."
   bash ${SCRIPT} ${OPENWRT_SDK}
 done
