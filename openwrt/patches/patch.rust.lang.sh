fd800873944eea7e239171e8a4c1baefa7cb3530
wangkexiong <wangkexiong@gmail.com>
2025-09-08 15:28:45 +0800

    correct llvm package link obsolete

---
+++ openwrt/patches/patch.rust.lang.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+OPENWRT_SDK="$1"
+
+RUST_MAKEFILE="${OPENWRT_SDK}/feeds/packages/lang/rust/Makefile"
+if [ -f "${RUST_MAKEFILE}" ]; then
+  sed -i 's/\(--set=llvm.download-ci-llvm=\)true/\1false/' "${RUST_MAKEFILE}"
+fi
