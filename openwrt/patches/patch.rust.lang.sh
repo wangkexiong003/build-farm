a5d721c186c1767a8552f513e8b4654e5213bc93
wangkexiong <wangkexiong@gmail.com>
2025-09-11 09:27:56 +0800

    fix debug condition

--- openwrt/patches/patch.rust.lang.sh
+++ openwrt/patches/patch.rust.lang.sh
@@ -4,7 +4,7 @@ OPENWRT_SDK="$1"
 
 RUST_MAKEFILE="${OPENWRT_SDK}/feeds/packages/lang/rust/Makefile"
 if [ -f "${RUST_MAKEFILE}" ]; then
-  if [ -f ${OPENWRT_SDK}/../.fresh-build ]; then
+  if [ ! -f ${OPENWRT_SDK}/../.fresh-build ]; then
     echo "+++++ patch file: ${RUST_MAKEFILE}"
     sed -i 's/\(--set=llvm.download-ci-llvm=\)true/\1false/' "${RUST_MAKEFILE}"
   fi
