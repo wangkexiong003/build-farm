1e807a594056786b529fd5bd7f28ddfc2a299094
wangkexiong <wangkexiong@gmail.com>
2025-09-12 13:58:03 +0800

    base for not trigger rust rebuild

--- openwrt/patches/patch.rust.lang.sh
+++ openwrt/patches/patch.rust.lang.sh
@@ -4,6 +4,8 @@ OPENWRT_SDK="$1"
 
 RUST_MAKEFILE="${OPENWRT_SDK}/feeds/packages/lang/rust/Makefile"
 if [ -f "${RUST_MAKEFILE}" ]; then
-  echo "+++++ patch file: ${RUST_MAKEFILE} +++++"
-  sed -i 's/\(--set=llvm.download-ci-llvm=\)true/\1false/' "${RUST_MAKEFILE}"
+  if [ -f ${OPENWRT_SDK}/../.fresh-build ]; then
+    echo "+++++ patch file: ${RUST_MAKEFILE} +++++"
+    sed -i 's/\(--set=llvm.download-ci-llvm=\)true/\1false/' "${RUST_MAKEFILE}"
+  fi
 fi
